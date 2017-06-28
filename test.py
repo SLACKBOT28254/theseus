caa61927022b8f11ea8b66b00f9b52117ede1ed2
Elena Falcone <elena.falcone@oakbrook.local>
2017-06-28 14:40:40 +0100

    change in requirement

--- test.py
+++ test.py
@@ -71,5 +71,6 @@ def test_arrangement_fee_questions_recognises_arrangement_fee_query():
     assert theseus.arrangement_fee_query("Admin fee") == True
     assert theseus.arrangement_fee_query("Will I be charged for admin?") == True
     assert theseus.arrangement_fee_query("Adamant is fear") == False
-   
+    assert theseus.arrangement_fee_query("arrange my loan") == False
+    assert theseus.arrangement_fee_query("Can I take a loan?") == False
     
